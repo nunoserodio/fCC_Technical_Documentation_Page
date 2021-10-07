dce6ec08dc4ad044d38d97202f56aba7352e5cd2
nunoserodio <34492208+nunoserodio@users.noreply.github.com>
2021-10-07 20:14:50 +0100

    Added burger menu for tablet/mobile.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,40 @@
 hljs.highlightAll();
+
+const hamburger = document.querySelector('#hamburger');
+const body = document.querySelector('body');
+const header = document.querySelector('.page-header');
+const fadeObjs = document.querySelectorAll('.fade');
+const menuBtn = document.querySelectorAll('.nav-link');
+
+function toggleHeader() {
+    if(header.classList.contains('open')){ // CLOSE HAMBURGER MENU
+      body.classList.remove('no-scroll');
+      header.classList.remove('open');
+      fadeObjs.forEach(function(element){
+        element.classList.remove('fade-in');
+        element.classList.add('fade-out');
+      });
+    }
+    else { // OPEN HAMBURGER MENU
+      //body.classList.add('no-scroll');
+      header.classList.add('open');
+      fadeObjs.forEach(function(element){
+        element.classList.remove('fade-out');
+        element.classList.add('fade-in');
+      });
+    }
+}
+
+hamburger.addEventListener('click', function(){
+  console.log('click hamburger');
+
+  toggleHeader()
+});
+
+menuBtn.forEach(button => {
+  button.addEventListener('click', function(){
+    console.log('click menuBtn');
+
+    toggleHeader()
+  })
+});
